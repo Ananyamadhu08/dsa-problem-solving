644a0b16b4a919ff2bd992aaa2cab2a4dc7ca640
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-02-06 02:27:49 +0700

    pattern 3 exercise

---
+++ patterns/pattern-3.js
@@ -0,0 +1,18 @@
+// Create a function to print the following pattern -
+// 1
+// 1 2
+// 1 2 3
+// 1 2 3 4
+// 1 2 3 4 5
+
+function printPattern(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += `${j} `;
+    }
+    console.log(row);
+  }
+}
+
+printPattern(5);
