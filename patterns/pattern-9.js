57ff1976125e11882fcff42305e38a0220e43c66
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-02-06 08:37:29 +0700

    pattern 9 exercise

---
+++ patterns/pattern-9.js
@@ -0,0 +1,42 @@
+// Create a function to print the following pattern -
+//     *
+//    ***
+//   *****
+//  *******
+// *********
+// *********
+//  *******
+//   *****
+//    ***
+//     *
+
+function printPattern(n) {
+  for (let i = 0; i < n; i++) {
+    let row = "";
+
+    for (let j = 0; j < n - i - 1; j++) {
+      row += " ";
+    }
+
+    for (let k = 0; k < i * 2 + 1; k++) {
+      row += "*";
+    }
+
+    console.log(row);
+  }
+
+  for (let i = 0; i < n; i++) {
+    let row = "";
+
+    for (let j = 0; j < i; j++) {
+      row += " ";
+    }
+
+    for (let k = 0; k < 2 * (n - i) - 1; k++) {
+      row += "*";
+    }
+    console.log(row);
+  }
+}
+
+printPattern(5);
