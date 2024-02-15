7d9ec92956cef7fd82263d73db798f6f62cd25b2
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-02-15 16:59:52 +0700

    check prime number brute force approach

---
+++ basic-math/check-prime-number/brute-force-approach.js
@@ -0,0 +1,13 @@
+// Create a function to determine if a given number is a prime number or not. The function will return true if it's a prime number or false otherwise.
+
+function isPrimeNumber(n) {
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) return false;
+  }
+
+  return true;
+}
+
+let result = isPrimeNumber(11);
+
+console.log(result);
