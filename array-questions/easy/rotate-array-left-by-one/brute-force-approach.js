faa2a14ff25fcaea9c6bf659062a227c157bfc33
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-03-18 00:48:49 +0530

    changed function names

--- array-questions/easy/rotate-array-left-by-one/brute-force-approach.js
+++ array-questions/easy/rotate-array-left-by-one/brute-force-approach.js
@@ -1,6 +1,6 @@
 // Perform a left rotation on an array of integers by one place. This means shifting each element of the array to the left by one position, and moving the first element to the end of the array.
 
-function rotateLeftArr(arr) {
+function rotateArrLeft(arr) {
   if (arr.length <= 1) return arr;
 
   const rotatedArr = [];
@@ -16,7 +16,7 @@ function rotateLeftArr(arr) {
 
 const numArr = [1, 2, 3, 4, 5];
 
-const result = rotateLeftArr(numArr);
+const result = rotateArrLeft(numArr);
 
 console.log(result);
 
