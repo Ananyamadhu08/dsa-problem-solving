c906cb412995e01052bea565b8e2469f1469684f
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-03-18 00:42:16 +0530

    rotate array left by one optimal approach

---
+++ array-questions/easy/rotate-array-left-by-one/optimal-approach.js
@@ -0,0 +1,24 @@
+// Perform a left rotation on an array of integers by one place. This means shifting each element of the array to the left by one position, and moving the first element to the end of the array.
+
+function rotateLeftArr(arr) {
+  if (arr.length <= 1) return arr;
+
+  const firstElement = arr[0];
+
+  for (let i = 1; i < arr.length; i++) {
+    arr[i - 1] = arr[i];
+  }
+
+  arr[arr.length - 1] = firstElement;
+
+  return arr;
+}
+
+const numArr = [1, 2, 3, 4, 5];
+
+const result = rotateLeftArr(numArr);
+
+console.log(result);
+
+// Time complexity: O(N)
+// Space complexity: O(1)
