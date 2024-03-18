941d1c3707cbb87f604b0709c3d9640bc070689d
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-03-18 06:32:31 +0530

    rotate array right by k elements optimal approach

---
+++ array-questions/easy/rotate-array-right-by-k-elements/optimal-approach.js
@@ -0,0 +1,33 @@
+// Rotate an array of integers to the right by k elements.
+
+function reverseArr(arr, start, end) {
+  while (start < end) {
+    [arr[start], arr[end]] = [arr[end], arr[start]];
+    start++;
+    end--;
+  }
+
+  return arr;
+}
+
+function rotateArrRight(arr, k) {
+  const n = arr.length;
+  k = k % n;
+
+  reverseArr(arr, 0, n - k - 1);
+
+  reverseArr(arr, n - k, n - 1);
+
+  reverseArr(arr, 0, n - 1);
+
+  return arr;
+}
+
+const numArr = [1, 2, 3, 4, 5];
+
+const result = rotateArrRight(numArr, 1);
+
+console.log(result);
+
+// Time complexity: O(N)
+// Space complexity: O(1)
