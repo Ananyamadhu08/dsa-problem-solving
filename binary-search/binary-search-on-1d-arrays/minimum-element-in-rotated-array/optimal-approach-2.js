70cbd41656afa3b2ff94c0f9515fbf3006444e26
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-04-30 04:12:19 +0530

    minimum element in rotated array optimal approach 2

---
+++ binary-search/binary-search-on-1d-arrays/minimum-element-in-rotated-array/optimal-approach-2.js
@@ -0,0 +1,41 @@
+// Given an integer array, sorted in ascending order with distinct values. The array is rotated at some pivot point unknown to you. Find the minimum element in the array.
+
+function findMinimumElement(arr) {
+  const n = arr.length;
+
+  let low = 0;
+  let high = n - 1;
+
+  let minimumElement = Infinity;
+
+  while (low <= high) {
+    let mid = Math.floor((low + high) / 2);
+
+    if (arr[low] <= arr[high]) {
+      minimumElement = Math.min(minimumElement, arr[low]);
+
+      break;
+    }
+
+    if (arr[low] <= arr[mid]) {
+      minimumElement = Math.min(minimumElement, arr[low]);
+
+      low = mid + 1;
+    } else {
+      minimumElement = Math.min(minimumElement, arr[mid]);
+
+      high = mid - 1;
+    }
+  }
+
+  return minimumElement;
+}
+
+const numArr = [4, 5, 1, 2, 3];
+
+const result = findMinimumElement(numArr);
+
+console.log(result);
+
+// Time complexity: O(log N)
+// Space complexity: O(1)
