4b7ea3c4b7632465fa79ada34c954c7f8011acc8
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-05-10 07:36:10 +0530

    aggressive cows brute force approach

---
+++ binary-search/binary-search-on-answers/aggressive-cows/brute-force-approach.js
@@ -0,0 +1,50 @@
+// given an array 'arr' of size 'n' which denotes the position of stalls. You are also given an integer 'k' which denotes the number of aggressive cows. Assign stalls to 'k' cows such that the minimum distance between any two of them is the maximum possible. Find the maximum possible minimum distance.
+
+function canWePlaceCows(stalls, distance, numOfCows) {
+  const n = stalls.length;
+
+  let countOfPlacedCows = 1;
+
+  let placementOfLastCow = stalls[0];
+
+  for (let i = 1; i < n; i++) {
+    if (stalls[i] - placementOfLastCow >= distance) {
+      countOfPlacedCows++;
+
+      placementOfLastCow = stalls[i];
+    }
+
+    if (countOfPlacedCows >= numOfCows) return true;
+  }
+
+  return false;
+}
+
+function calculateDistance(stalls, numOfCows) {
+  const n = stalls.length;
+
+  stalls.sort((a, b) => a - b);
+
+  const maxLimit = stalls[n - 1] - stalls[0];
+
+  for (let i = 1; i <= maxLimit; i++) {
+    let checkCanPlaceCows = canWePlaceCows(stalls, i, numOfCows);
+
+    if (checkCanPlaceCows === false) {
+      return i - 1;
+    }
+  }
+
+  return maxLimit;
+}
+
+const stalls = [0, 3, 4, 7, 10, 9];
+
+const numOfCows = 4;
+
+const result = calculateDistance(stalls, numOfCows);
+
+console.log(result);
+
+// Time complexity: O(N log N) + O(N * maxLimit)
+// Space complexity: O(1)
