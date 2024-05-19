fdbb3670a06c2811ebc200d545a73a86aa52f827
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-05-19 15:23:09 +0530

    find element in sorted matrix brute force approach

---
+++ binary-search/binary-search-on-2d-arrays/find-element-in-sorted-matrix/brute-force-approach.js
@@ -0,0 +1,31 @@
+// given a non-empty matrix with 'n' rows and 'm' columns. All the rows are sorted in ascending order. The first element of a row is greater than the last element of the previous row if it exists. You are given a target, find if it exists in the given matrix or not.
+
+function findElementInMatrix(matrix, target) {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      if (matrix[i][j] == target) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+const matrix = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+const target = 5;
+
+const result = findElementInMatrix(matrix, target);
+
+console.log(result);
+
+// Time complexity: O(row * cols)
+// Space complexity: O(1)
